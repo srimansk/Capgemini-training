8c81c69556445e4c6e7af781a54a93af3cd5a914
srimansk <s.k.sriman007@gmail.com>
2026-06-30 12:27:04 +0530

    Update ArrayCopy.java

--- ArrayCopy.java
+++ ArrayCopy.java
@@ -4,15 +4,11 @@ public class ArrayCopy {
 	    public static void main(String[] args) {
 	        int[] original = {1, 2, 3, 4, 5};
 
-	        // Method 1: Using Arrays.copyOf (The easiest way)
-	        // This creates a new array and copies the old one into it
 	        int[] copy1 = Arrays.copyOf(original, original.length);
 
-	        // Method 2: Using System.arraycopy (The manual way)
 	        int[] copy2 = new int[original.length];
 	        System.arraycopy(original, 0, copy2, 0, original.length);
 
-	        // Prove it works: Modify the copy
 	        copy1[0] = 99;
 	        copy2[3] = 34;
 
